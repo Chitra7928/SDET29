ead78c6e26672120dbd687b27d31c0213192786d
NEW <NEW@DESKTOP-MQK53ML>
2022-01-24 21:39:47 +0530

    user 1 second commit

--- src/demoGit/Demogit.java
+++ src/demoGit/Demogit.java
@@ -5,6 +5,7 @@ public class Demogit {
 	public static void main(String[] args) {
 		System.out.println("user 1 commit ");
 		System.out.println("Hi this is user 2 first commit");
+		System.out.println("Hi this is user 1 second commit");
 	}
 
 }
